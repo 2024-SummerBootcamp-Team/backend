9eec0764c59a4764b8438576b53a6294a1e02468
kalsteve <kalgansteve@gmail.com>
2024-07-02 16:46:45 +0900

    feat: 엔드포인트 경로 수정

--- app/routers/users.py
+++ app/routers/users.py
@@ -12,21 +12,21 @@ router = APIRouter(
 )
 
 
-@router.get("/users/", response_model=list[usersSchema.User])
+@router.get("", response_model=list[usersSchema.User])
 def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(session.get_db)):
     db_users = crud.get_users(db, skip=skip, limit=limit)
     return db_users
 
 
-@router.post("/users/", response_model=usersSchema.User)
+@router.post("", response_model=usersSchema.User)
 def create_user(user: usersSchema.User, db: Session = Depends(session.get_db)):
     db_user = crud.get_user_by_name(db, name=user.name)
     if db_user:
-        raise HTTPException(status_code=400, detail="Email already registered")
+        raise HTTPException(status_code=400, detail="User name already registered")
     return crud.create_user(db=db, user=user)
 
 
-@router.get("/users/{user_id}", response_model=usersSchema.User)
+@router.get("/{user_id}", response_model=usersSchema.User)
 def read_user(user_id: int, db: Session = Depends(session.get_db)):
     db_user = crud.get_user(db, user_id=user_id)
     if db_user is None:
