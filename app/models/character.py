44322b5b709c081dd7b3d04fd8f7bff6e70f90d2
dlwhsk0 <hana.dev.314@gmail.com>
2024-07-07 21:32:05 +0900

    feat: 모델 정의 및 예시 user 파일 삭제

---
+++ app/models/character.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+
+from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey, Text
+from ..database.session import Base
+
+
+# from sqlalchemy.orm import relationship
+
+
+class Character(Base):
+    __tablename__ = 'character'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    is_deleted = Column(Boolean, default=False, nullable=False)
+    created_at = Column(DateTime, nullable=False, default=datetime.now)
+    updated_at = Column(DateTime, nullable=False, default=datetime.now, onupdate=datetime.now)
+    name = Column(String(20), nullable=False)
+    prompt = Column(Text, nullable=False)
+
+    # chats = relationship("Chat", back_populates="character")
