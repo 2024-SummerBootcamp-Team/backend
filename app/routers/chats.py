c902d9f27527c39e990016795f4768c6249af172
김홍석 <drghdtjr@naver.com>
2024-07-08 03:18:44 +0900

    feat:마지막수정

--- app/routers/chats.py
+++ app/routers/chats.py
@@ -22,11 +22,12 @@ def read_chat_room(chat_id: int, db: Session = Depends(get_db)):
 
     return chat_room
 
-@router.get("/bubbles/{chat_id}", response_model=ChatBubbleList)
+@router.get("/{chat_id}/bubbles", response_model=ChatBubbleList)
 def read_chat_bubble(chat_id: int, db: Session = Depends(get_db)):
-    chat_bubble = crud.get_bubbles(db, chat_id=chat_id)
-    if not chat_bubble:
+    chat_room = crud.get_chat_room(db, chat_room_id=chat_id)
+    if not chat_room:
         raise HTTPException(status_code=404, detail="채팅방 정보를 불러오는데 실패했습니다.")
+    chat_bubble = crud.get_bubbles(db, chat_id=chat_id)
 
 
     return chat_bubble
