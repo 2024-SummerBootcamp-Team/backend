abc7633bb35deae6f53c85f83ef48ae04bd6763e
kalsteve <kalgansteve@gmail.com>
2024-07-08 17:55:32 +0900

    set: cicd sets

--- scripts/start.sh
+++ scripts/start.sh
@@ -9,37 +9,37 @@ DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
 TIME_NOW=$(date +%c)
 
-EXIST_DOCKER_APP = $(docker ps -a | grep $DOCKER_APP_NAME)
-
-cd $PROJECT_ROOT
+EXIST_DOCKER_APP=$(docker ps -a | grep $DOCKER_APP_NAME)
 
 # docker-compose 파일 실행
 # docker compose가 실행 중이면 종료
-if [ -z "EXIST_DOCKER_APP" ]; then
+if [ -z "$EXIST_DOCKER_APP" ]; then
 
-# docker-compose 파일 실행
-echo "$TIME_NOW > $PROJECT_ROOT docker-compose 파일 실행" >> $DEPLOY_LOG
-docker-compose -p ${DOCKER_APP_NAME} -f docker-compose.yml up -d --build
+    # docker-compose 파일 실행
+    echo "$TIME_NOW > $PROJECT_ROOT docker-compose 파일 실행" >> $DEPLOY_LOG
+    docker-compose -p ${DOCKER_APP_NAME} -f docker-compose.yml up -d --build
 
-sleep 10
+    sleep 20
 
 else
 
-# docker-compose 파일 종료
-echo "$TIME_NOW > $PROJECT_ROOT docker-compose 파일 종료" >> $DEPLOY_LOG
-docker-compose -p ${DOCKER_APP_NAME} -f docker-compose.yml down
+    # docker-compose 파일 종료
+    echo "$TIME_NOW > $PROJECT_ROOT docker-compose 파일 종료" >> $DEPLOY_LOG
+    docker-compose -p ${DOCKER_APP_NAME} -f docker-compose.yml down
 
-# docker-compose 파일 실행
-echo "$TIME_NOW > $PROJECT_ROOT docker-compose 파일 실행" >> $DEPLOY_LOG
-docker-compose -p ${DOCKER_APP_NAME} -f docker-compose.yml up -d --build
+    # docker-compose 파일 실행
+    echo "$TIME_NOW > $PROJECT_ROOT docker-compose 파일 실행" >> $DEPLOY_LOG
+    docker-compose -p ${DOCKER_APP_NAME} -f docker-compose.yml up -d --build
+
+    sleep 20
 
 fi
 
-CURRENT_PID=$(pgrep -f DOCKER_APP_NAME)
+CURRENT_PID=$(pgrep -f $DOCKER_APP_NAME)
 echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
 
 echo "$TIME_NOW > 배포 종료" >> $DEPLOY_LOG
 
 echo "===================== 배포 완료 =====================" >> $DEPLOY_LOG
 
-echo >> /home/ec2-user/app/deploy.log
+echo >> $DEPLOY_LOG
