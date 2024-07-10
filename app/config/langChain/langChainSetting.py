1d49d96d88a0bf2989e0c3b136ddde53a7edf665
kalsteve <kalgansteve@gmail.com>
2024-07-10 20:24:01 +0900

    set: LangChain 채팅 저장 임시구현

---
+++ app/config/langChain/langChainSetting.py
@@ -0,0 +1,31 @@
+import app.config.envSetting
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_community.chat_message_histories import SQLChatMessageHistory
+import os
+
+llm = ChatOpenAI(model="gpt-3.5-turbo-0125")
+
+DATABASE_URL = os.environ.get('DATABASE_URL')
+DATABASE_USERNAME = os.environ.get('DATABASE_USERNAME')
+DATABASE_PASSWORD = os.environ.get('DATABASE_PASSWORD')
+DATABASE_PORT = os.environ.get('DATABASE_PORT')
+DATABASE_DBNAME = os.environ.get('DATABASE_DBNAME')
+
+
+SQLALCHEMY_DATABASE_URL = f'mysql+aiomysql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_URL}:{DATABASE_PORT}/{DATABASE_DBNAME}'
+
+
+def get_session_history(session_id):
+    return SQLChatMessageHistory(session_id=session_id,
+                                 connection=SQLALCHEMY_DATABASE_URL,
+                                 async_mode=True,
+                                 )
+
+
+runnable_with_history = RunnableWithMessageHistory(
+    llm,
+    get_session_history,
+)
+
