3f2efd281f3889927a3dc2a4e76f335d85fd2925
김홍석 <drghdtjr@naver.com>
2024-07-12 23:03:13 +0900

    feat:모니터링기능

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 import app.config.envSetting # 환경 변수를 가져오기 위한 설정
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
+from prometheus_fastapi_instrumentator import Instrumentator
 
 from .routers import api
-
 
 # FastAPI를 실행하기 위해 인스턴스 생성
 app = FastAPI()
@@ -16,6 +16,11 @@ app.add_middleware(
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],  # 허용할 메서드들 설정
     allow_headers=["*"],  # 모든 헤더를 허용
 )
+
+Instrumentator().instrument(app).expose(app)
+
+
+
 
 # 라우팅 설정
 """
