a3d5ac8a6263ed83d9218b669e96b241f0dfa079
김홍석 <drghdtjr@naver.com>
2024-07-18 01:56:59 +0900

    feat:일단 파일 위치 바꾸기전

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 from prometheus_fastapi_instrumentator import Instrumentator
 
+
 from .routers import api
 
 # FastAPI를 실행하기 위해 인스턴스 생성
@@ -27,10 +28,9 @@ app.add_middleware(
 instrumentator = Instrumentator().instrument(app)
 instrumentator.expose(app, include_in_schema=False)
 
-
-
-
 # 라우팅 설정
+
+
 """
 라우트란?
 - 클라이언트로부터 요청을 받았을 때, 해당 요청을 처리할 수 있는 함수를 매핑하는 것
