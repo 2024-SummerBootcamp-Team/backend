e5d94f59385048170c4a43e276499fbafaf247c2
dlwhsk0 <hana.dev.314@gmail.com>
2024-07-21 06:36:44 +0900

    feat: 캐릭터 목록 조회 api 수정

--- app/routers/characters.py
+++ app/routers/characters.py
@@ -16,4 +16,4 @@ router = APIRouter(
 @router.get("", response_model=ResultResponseModel, summary="캐릭터 목록 조회", description="캐릭터 목록 및 정보를 조회합니다.")
 def read_characters(db: Session = Depends(get_db), skip: int = 0, limit: int = 100):
     characters = character_service.get_characters(db, skip=skip, limit=limit)
-    return ResultResponseModel(code=200, message="저장된 모든 목소리 목록을 조회했습니다.", data=characters)
+    return ResultResponseModel(code=200, message="캐릭터 목록을 조회했습니다.", data=CharacterList(characters=characters))
